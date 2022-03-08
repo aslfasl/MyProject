bbc8d10d65111e460e04929a6415e6b9b0c36e1d
VolkovAndrey <soupec@yandex.ru>
2022-03-08 14:31:59 +0300

    UserServiceImp. UserController created

---
+++ src/main/java/com/example/project/service/UserServiceImp.java
@@ -0,0 +1,54 @@
+package com.example.project.service;
+
+import com.example.project.entity.AppUser;
+import com.example.project.entity.Role;
+import com.example.project.repo.RoleRepo;
+import com.example.project.repo.UserRepo;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+@Slf4j // TODO: 08.03.2022
+public class UserServiceImp implements UserService{
+
+    private final UserRepo userRepo;
+    private final RoleRepo roleRepo;
+
+    @Override
+    public AppUser saveUser(AppUser user) {
+        log.info("Saving new user {} to the database", user.getName());
+        return userRepo.save(user);
+    }
+
+    @Override
+    public Role saveRole(Role role) {
+        log.info("Saving new role {} to the database", role.getName());
+        return roleRepo.save(role);
+    }
+
+    @Override
+    public void addRoleToUser(String username, String roleName) {
+        log.info("Adding role {} to user {}", roleName, username);
+        AppUser user = userRepo.findByUsername(username);
+        Role role = roleRepo.findByName(roleName);
+        user.getRoles().add(role);
+    }
+
+    @Override
+    public AppUser getUser(String username) {
+        log.info("Fetching user {}", username);
+        return userRepo.findByUsername(username);
+    }
+
+    @Override
+    public List<AppUser> getUsers() {
+        log.info("Fetching all users");
+        return userRepo.findAll();
+    }
+}
