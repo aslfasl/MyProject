3d269db4bb1ac5453610bbffa18445bb79c4a2b9
VolkovAndrey <soupec@yandex.ru>
2022-03-14 20:02:34 +0300

    Fixed one repo test. Test Exception Handler task

--- src/test/java/com/example/project/repo/ClientRepoTest.java
+++ src/test/java/com/example/project/repo/ClientRepoTest.java
@@ -1,11 +1,13 @@
 package com.example.project.repo;
 
 import com.example.project.entity.ClientEntity;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,12 +17,20 @@ class ClientRepoTest {
     @Autowired
     private ClientRepo clientRepo;
 
+    @BeforeEach
+    void setUp(){
+        clientRepo.deleteAll();
+    }
+
     @Test
-    void findAllByActive() {
-        // TODO: 10.03.2022
+    void findAllByActiveIsTrue() {
         clientRepo.save(new ClientEntity("test", "test", "1234", LocalDate.of(2000,1,1), true));
         clientRepo.save(new ClientEntity("test", "test", "12234", LocalDate.of(2000,1,1), true));
         clientRepo.save(new ClientEntity("test", "test", "12354", LocalDate.of(2000,1,1), false));
-        System.out.println(clientRepo.findAllByIsActiveTrue());
+
+        List<ClientEntity> allByIsActiveTrue = clientRepo.findAllByIsActiveTrue();
+
+        assertTrue(allByIsActiveTrue.stream().allMatch(ClientEntity::isActive));
+        assertEquals(2, allByIsActiveTrue.size());
     }
 }
