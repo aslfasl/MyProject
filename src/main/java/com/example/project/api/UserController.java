7fdfcf9852bdc178777fe0a8c6a6791c9c3c62f0
VolkovAndrey <soupec@yandex.ru>
2022-03-17 12:29:07 +0300

    SecurityService added. Logic removed from Controller to Service.

--- src/main/java/com/example/project/api/UserController.java
+++ src/main/java/com/example/project/api/UserController.java
@@ -6,6 +6,7 @@ import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.example.project.entity.AppUser;
 import com.example.project.entity.Role;
+import com.example.project.service.SecurityService;
 import com.example.project.service.UserService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Data;
@@ -36,6 +37,7 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 public class UserController {
 
     private final UserService userService;
+    private final SecurityService securityService;
 
     @GetMapping("/user/all")
     public ResponseEntity<List<AppUser>> getUsers() {
@@ -60,42 +62,9 @@ public class UserController {
         return ResponseEntity.ok().build();
     }
 
-    // TODO: 16.03.2022 move logic from controller to service
     @GetMapping("/token/refresh")
     public void refreshToken(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String authorizationHeader = request.getHeader(AUTHORIZATION);
-        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
-            try {
-                String refreshToken = authorizationHeader.substring("Bearer ".length());
-                Algorithm algorithm = Algorithm.HMAC256("secret".getBytes());
-                JWTVerifier verifier = JWT.require(algorithm).build();
-                DecodedJWT decodedJWT = verifier.verify(refreshToken);
-                String username = decodedJWT.getSubject();
-                AppUser user = userService.getUser(username);
-                String accessToken = JWT.create()
-                        .withSubject(user.getUsername())
-                        .withExpiresAt(new Date(System.currentTimeMillis() + 20 * 60 * 1000))
-                        .withIssuer(request.getRequestURL().toString())
-                        .withClaim("roles", user.getRoles().stream()
-                                .map(Role::getName)
-                                .collect(Collectors.toList()))
-                        .sign(algorithm);
-                Map<String, String> tokens = new HashMap<>();
-                tokens.put("access_token", accessToken);
-                tokens.put("refresh_token", refreshToken);
-                response.setContentType(APPLICATION_JSON_VALUE);
-                new ObjectMapper().writeValue(response.getOutputStream(), tokens);
-            } catch (Exception exception) {
-                response.setHeader("error", exception.getMessage());
-                response.setStatus(FORBIDDEN.value());
-                Map<String, String> error = new HashMap<>();
-                error.put("error_message", exception.getMessage());
-                response.setContentType(APPLICATION_JSON_VALUE);
-                new ObjectMapper().writeValue(response.getOutputStream(), error);
-            }
-        } else {
-            throw new RuntimeException("refresh token is missing"); // TODO: 09.03.2022 Custom exception?
-        }
+        securityService.refreshToken(request, response);
     }
 }
 
